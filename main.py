5f385e53249788702d3b428d24208e1caf815f7c
pogdogpy <matteochoularton@gmail.com>
2023-03-11 15:35:46 +0000

    added colorama im autism

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import time
 import random
+import colorama
+from colorama import Fore
+sel = input(Fore.MAGENTA + "Welcome to my number guessing game would you like to guess (a) 1-10 (b) 1-100 ")
 
-sel = input("Welcome to my number guessing game would you like to guess (a) 1-10 (b) 1-100 ")
 
-if sel == "a":
+ if sel == "a":
     num1 = random.randint(1, 10)
-    ans1 = int(input("Pick a number 1-10 "))
+    ans1 = int(input(Fore.RED+"Pick a number 1-10 ---> "))
     if ans1 == num1:
-        print("You Guessed Right!")
-        time.sleep(3)
-        quit()
+        print(Fore.LIGHTMAGENTA_EX+"You Guessed Right!")
     else:
-        print("YOU GUESSED WRONG, Try Again!")
-        print("Your Number was " + str(num1))
-        time.sleep(3)
-        quit()
+        print(Fore.CYAN+"You guessed Wrong.")
+        time.sleep(0.6)
+        print("Your Number Was " + str(num1))
+    
+   
         
 
 if sel == "b":
     num2 = random.randint(1, 100)
-    ans2 = int(input("Guess a number 1-100 "))
+    ans2 = int(input(Fore.CYAN+"Guess a number 1-100 ---> "))
     if ans2 == num2:
-        print("Holy Hell You Got it right?!?!?")
+        print(Fore.RED+"Holy Hell You Got it right?!?!?")
         time.sleep(3)
         quit()
     else:
-        print("WRONG")
-        print("Your Number Was" +  str(num2))
+        print(Fore.YELLOW+"WRONG")
+        time.sleep(0.9)
+        print(Fore.GREEN+"Your Number Was " +  str(num2))
         time.sleep(2)
         quit()
 
